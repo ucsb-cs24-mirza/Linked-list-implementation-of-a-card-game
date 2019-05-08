23e8509a036487b7ae8b908fef56dfaff59304be
Danming Wang <danming@ucsb.edu>
2019-05-08 11:28:34 -0700

    implemented version

--- cards.h
+++ cards.h
@@ -6,6 +6,7 @@
 #define CARDS_H
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Card {
@@ -20,17 +21,18 @@ class CardList {
 
 public:
     CardList():first(nullptr){}
+    ~CardList();
     void setPlayer(string n){player=n;}
     Card* getfirst();
-    Card* get(string i);
+//    Card* get(string i);
     void append(string i); // append value at end of list
-    void print() const;     // print values separate by ' '
+    void print() const;     // print values in the list
 
-    ~CardList();                      // destructor
     CardList(const CardList& source); //copy constructor (deep copy)
     bool contains(string i) const;  // true if value in list
     void deleteCard(string i); // delete the matching card from the CardList
-    CardList& operator=(const CardList& source); //overloaded  (NO CHANGE)
+    CardList& operator=(const CardList& source); 
+    vector<string> vectorize();
     friend ostream& operator<<(ostream& os, const CardList& random){
         os<< random.player;
         return os;
@@ -39,20 +41,9 @@ public:
 
 private:
     string player="default";
-    Card *first; // pointer to first node (DO NOT CHANGE):
+    Card *first; 
 };
 
 void play(CardList& l1, CardList& l2);
 #endif
 
-// Use this file to define all your classes and public functions
-// You should design your program with OOP prinicples using classes
-// An example of class design would be having Card, CardList and Player as Classes.
-// Card represent Card in your LinkedList which has some data in it
-// CardList can be your LinkedList Class which has operations such as append, remove etc
-// Player class represents your game player
-// Think about the public functions that should be part of each class. (All public function need to be tested in unit testing)
-// One of the important functions is search / find which is essential for players to find cars in their opponent's "hand"
-// Make sure you overload the desired operators
-// This is not the fixed design. We are not expecting exactly 3 classes. You can do with less or more.
-// Important thing to consider is abstraction.
